257800892011b98f6e253af5fb10763fd38fe4b5
Adarsh Vishwakarma <vish.adarsh01@gmail.com>
2024-10-27 01:53:21 +0530

    google verification meta tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import {Recursive} from "next/font/google"
 import "./globals.css";
+import Head from 'next/head';
+
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -17,6 +19,7 @@ const recursive = Recursive({ subsets: ["cyrillic-ext"] });
 export const metadata: Metadata = {
   title: "Adarsh Vishwakarma",
   description: "Custom Portfolio Website for Adarsh Vishwakarma to showcase his work and projects.",
+
 };
 
 export default function RootLayout({
@@ -26,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="qeJyPSM_Y9EMoJsyleMcixsWmwRJmMqt05yJelGLERY" />
+      </Head>
       <body
         className={recursive.className}
       >
